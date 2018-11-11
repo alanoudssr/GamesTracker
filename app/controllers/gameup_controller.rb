2a8342585875f8214f971352c7d67cbec18d4840
alanoudssr <alanoudssr@gmail.com>
2018-11-11 19:19:44 +0300

    added search feature

--- app/controllers/gameup_controller.rb
+++ app/controllers/gameup_controller.rb
@@ -4,4 +4,11 @@ class GameupController < ApplicationController
     client = IGDB::Client.new ENV["IGDB_API_KEY"]
     @results = client.games 1942, {fields: "name"}
   end
+
+  def search
+    client = IGDB::Client.new ENV["IGDB_API_KEY"]
+
+    @term = params[:term]
+    @result = client.search_games @term, {fields: "name,release_dates,esrb.synopsis,rating"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "gameup#index"
+  get "/search", to: "gameup#search"
   get "gameup/index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
